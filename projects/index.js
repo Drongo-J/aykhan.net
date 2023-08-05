0e3dc52dbd92a727d0e90df19964a2305dfc9979
Aykhan <ayxanx17@gmail.com>
2023-08-05 21:13:13 +0400

    Change University to Koc University, Added Zust to Projects

--- projects/index.js
+++ projects/index.js
@@ -3,6 +3,9 @@ var container = document.getElementById("projects");
 const khanChess = new PageLinkItem(1, "KhanChess", "Chess game agains chess bot",
     "https://media.aykhan.net/thumbnails/projects/khanchess.png", "https://aykhan.net/projects/khanchess");
 
+const zust = new PageLinkItem(2, "Zust", "Social Media Website",
+    "https://media.aykhan.net/thumbnails/projects/zust.png", "https://aykhan.net/projects/zust");
+
 const projects = [khanChess];
 
 addItemsToContainer(container, projects);
