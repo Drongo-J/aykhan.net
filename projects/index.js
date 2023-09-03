dcffaf058d7241207d49b1aabc041949c8bf091d
Aykhan <ayxanx17@gmail.com>
2023-09-03 11:14:02 +0400

    Added KhanLibrary React Exam Project

--- projects/index.js
+++ projects/index.js
@@ -6,6 +6,9 @@ const khanChess = new PageLinkItem(1, "KhanChess", "Chess game agains chess bot"
 const zust = new PageLinkItem(2, "Zust", "Social Media Website",
     "https://media.aykhan.net/thumbnails/projects/zust.png", "https://aykhan.net/projects/zust");
 
-const projects = [khanChess, zust];
+const khanLibrary = new PageLinkItem(3, "KhanLibrary", "Library App in React",
+"https://media.aykhan.net/thumbnails/projects/khanlibrary.png", "https://aykhan.net/projects/khanlibrary");
+
+const projects = [khanChess, zust, khanLibrary];
 
 addItemsToContainer(container, projects);
