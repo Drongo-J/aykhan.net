6209e49c1a23bbdc8e6b5abfac4970b608473fb3
Aykhan Ahmadzada <ayxanx17@gmail.com>
2023-09-18 15:06:54 +0400

    Updated Home Page Image

--- projects/index.js
+++ projects/index.js
@@ -1,6 +1,6 @@
 var container = document.getElementById("projects");
 
-const khanChess = new PageLinkItem(1, "KhanChess", "Chess game agains chess bot",
+const khanChess = new PageLinkItem(1, "KhanChess", "Playing a chess game against a chess bot that chooses moves randomly.",
     "https://media.aykhan.net/thumbnails/projects/khanchess.png", "https://aykhan.net/projects/khanchess");
 
 const zust = new PageLinkItem(2, "Zust", "Social Media Website",
